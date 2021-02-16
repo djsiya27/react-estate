5e97f6cf823f233846ffb1dfa7bf138059741bc5
Siyabonga Majola <siyabongamavundla@yahoo.com>
2021-02-16 19:43:41 +0200

    added navigation and slider

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,92 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components/macro";
 import { Link } from "react-router-dom";
+import { menuData } from "../data/MenuData";
+import { Button } from "./Button";
+import { FaBars } from "react-icons/fa";
 
 const Nav = styled.nav`
   height: 60px;
-  background: red;
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+  z-index: 100;
+  position: fixed;
+  width: 100%;
+`;
+
+const NavLink = css`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  text-decoration: none;
 `;
 
 const Logo = styled(Link)`
-  color: #fff;
+  ${NavLink}
+  font-style: italic;
 `;
 
-const MenuBars = styled.i``;
+const MenuBars = styled(FaBars)`
+  display: none;
 
-const NavMenu = styled.div``;
+  @media screen and (max-width: 768px) {
+    color: #fff;
+    display: block;
+    height: 40px;
+    width: 40px;
+    cursor: pointer;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-50px, 25%);
+  }
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: --48px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const NavMenuLinks = styled(Link)`
+  ${NavLink}
+`;
+
+const NavBtn = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
 
 const Navbar = () => {
   return (
     <Nav>
-      <Logo>HOME</Logo>
+      <Logo to="/">HOME</Logo>
       <MenuBars />
-      <NavMenu></NavMenu>
-      <h1>Navbar</h1>
+      <NavMenu>
+        {menuData.map((item, index) => (
+          <NavMenuLinks to={item.link} key={index}>
+            {item.title}
+          </NavMenuLinks>
+        ))}
+      </NavMenu>
+      <NavBtn>
+        <Button to="/contact" primary="true">
+          Contact Us
+        </Button>
+      </NavBtn>
     </Nav>
   );
 };
---
+++ src/data/SliderData.js
@@ -0,0 +1,61 @@
+export const SliderData = [
+  {
+    title: "Luxury Villa in Bali, Indonesia",
+    price: "$4,280,000",
+    path: "/homes",
+    label: "View Home",
+    image: "/images/ImageOne.jpg",
+    alt: "House",
+  },
+  {
+    title: "House in San Jose, Costa Rica",
+    price: "$2,860,000",
+    path: "/homes",
+    label: "View Home",
+    image: "/images/ImageTwo.jpg",
+    alt: "House",
+  },
+  {
+    title: "House in Calabasas, California",
+    price: "$8,420,000",
+    path: "/homes",
+    label: "View Home",
+    image: "/images/ImageThree.jpg",
+    alt: "House",
+  },
+  {
+    title: "House in Houston, Texas",
+    price: "$6,660,000",
+    path: "/homes",
+    label: "View Home",
+    image: "/images/ImageFour.jpg",
+    alt: "House",
+  },
+];
+
+// export const SliderDataTwo = [
+//   {
+//     title: "House in Las Vegas, Nevada",
+//     price: "$8,980,000",
+//     path: "/homes",
+//     label: "View Home",
+//     image: ImageFive,
+//     alt: "House",
+//   },
+//   {
+//     title: "House in Houston, Texas",
+//     price: "$3,460,000",
+//     path: "/homes",
+//     label: "View Home",
+//     image: ImageSix,
+//     alt: "House",
+//   },
+//   {
+//     title: "House in Alajuela, Costa Rica",
+//     price: "$5,420,000",
+//     path: "/homes",
+//     label: "View Home",
+//     image: ImageSeven,
+//     alt: "House",
+//   },
+// ];
