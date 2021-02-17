3caf9473c445d39ce730229f3911e90e1d9e5273
Siyabonga Majola <siyabongamavundla@yahoo.com>
2021-02-17 12:57:25 +0200

    Added the content section

---
+++ src/data/InfoData.js
@@ -0,0 +1,24 @@
+export const InfoData = {
+  heading: "Explore our beautiful homes",
+  paragraphOne:
+    "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose.",
+  paragraphTwo:
+    "making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text.",
+  buttonLabel: "View Homes",
+  image: "/images/ImageOne.jpg",
+  alt: "House",
+  reverse: false,
+  delay: 100,
+};
+export const InfoDataTwo = {
+  heading: "Morden Designs",
+  paragraphOne:
+    "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose.",
+  paragraphTwo:
+    "making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text.",
+  buttonLabel: "View Homes",
+  image: "/images/ImageTwo.jpg",
+  alt: "House",
+  reverse: true,
+  delay: 300,
+};
